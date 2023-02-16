e331fd712a58518c40d2ff1e9c74a6438e10aae5
Julio Camargo <jcamargo@hotmail.co.uk>
2023-02-16 17:30:04 +0000

    working on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,22 @@
 import styles from "@/styles/Home.module.css";
-
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className={styles.main}> 
-      <h1>Home</h1>
+      <div className={styles.info}>
+        <h2 className={styles.h2}>NEW PRODUCT</h2>
+        <h1 className={styles.h1}>XX99 MARK II HEADPHONES</h1>
+        <p className={styles.p}>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+      </div>
+      <div className={styles.image}>
+        <Image
+        src='/assets/home/desktop/hero.jpeg'
+        width={250}
+        height={600}
+        alt='XX99 MARK II HEADPHONES'
+        />
+      </div>
     </div>
   );
 }
